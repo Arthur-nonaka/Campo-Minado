8da4ce2a41fc75918f099688deb324190e80518e
Arthur <zenaboa7@gmail.com>
2023-06-25 19:46:36 -0300

    beta

--- src/App.js
+++ src/App.js
@@ -1,56 +1,130 @@
+import { useState, useEffect } from "react";
+
 import ShowSquare from "./components/ShowSquare";
 
 function App() {
+  const [bombs, setBombs] = useState([]);
+  const [reset, setReset] = useState(true);
+  const [action, setAction] = useState("bomba");
 
-    let bombs = [];
+  useEffect(() => {
+    let updatedBombs = [];
     for (let j = 0; j < 10; j++) {
-        bombs[j] = [];
-        for (let x = 0; x < 10; x++) {
-            if (Math.random() * 10 > 8) {
-                bombs[j][x] = 1;
-            }
-            else bombs[j][x] = 0;
-        }
+      updatedBombs[j] = [];
+      for (let x = 0; x < 10; x++) {
+        if (Math.random() * 10 > 8) {
+          updatedBombs[j][x] = 10;
+        } else updatedBombs[j][x] = 9;
+      }
     }
+    setBombs(updatedBombs);
+  }, [reset]);
+  console.log(bombs);
 
-    const handleClick = (x, j) => {
-        let amount = 0;
-        if(bombs[x-1][j] === 1) {
-            amount++;
+  const handleClick = async (x, j, square) => {
+    let amount = 0;
+    if (action === "bomba") {
+      if (square === 10) {
+        alert("Morreu");
+        amount = -1;
+        setReset(!reset);
+      } else {
+        if (
+          (x !== 0 && j !== 0 && bombs[j - 1][x - 1] === 10) ||
+          (x !== 0 && j !== 0 && bombs[j - 1][x - 1] === 12)
+        ) {
+          amount++;
         }
-        if(bombs[x-1][j-1] === 1) {
-            amount++;
+        if (
+          (x !== 0 && bombs[j][x - 1] === 10) ||
+          (x !== 0 && bombs[j][x - 1] === 12)
+        ) {
+          amount++;
         }
-        if(bombs[x][j-1] === 1) {
-            amount++;
+        if (
+          (x !== 0 && j !== 9 && bombs[j + 1][x - 1] === 10) ||
+          (x !== 0 && j !== 9 && bombs[j + 1][x - 1] === 12)
+        ) {
+          amount++;
         }
-        if(bombs[x+1][j-1] === 1) {
-            amount++;
+        if (
+          (j !== 0 && bombs[j - 1][x] === 10) ||
+          (j !== 0 && bombs[j - 1][x] === 12)
+        ) {
+          amount++;
         }
-        if(bombs[x+1][j] === 1) {
-            amount++;
+        if (
+          (j !== 9 && bombs[j + 1][x] === 10) ||
+          (j !== 9 && bombs[j + 1][x] === 12)
+        ) {
+          amount++;
         }
-        if(bombs[x+1][j+1] === 1) {
-            amount++;
+        if (
+          (j !== 9 && bombs[j + 1][x + 1] === 10) ||
+          (j !== 9 && bombs[j + 1][x + 1] === 12)
+        ) {
+          amount++;
         }
-        if(bombs[x][j+1] === 1) {
-            amount++;
+        if (bombs[j][x + 1] === 10 || bombs[j][x + 1] === 12) {
+          amount++;
         }
-        if(bombs[x-1][j+1] === 1) {
-            amount++;
+        if (
+          (j !== 0 && bombs[j - 1][x + 1] === 10) ||
+          (j !== 0 && bombs[j - 1][x + 1] === 12)
+        ) {
+          amount++;
         }
+      }
+      if (amount === 0) {
+        
+      }
+      let updatedBombs = [...bombs];
+      updatedBombs[j][x] = amount;
+      setBombs(updatedBombs);
+    } else {
+      let updatedBombs = [...bombs];
+      if (updatedBombs[j][x] === 12 || updatedBombs[j][x] === 11) {
+        if (updatedBombs[j][x] === 12) {
+          updatedBombs[j][x] = 10;
+        }
+        if (updatedBombs[j][x] === 11) {
+          updatedBombs[j][x] = 9;
+        }
+      } else {
+        if (updatedBombs[j][x] === 10) {
+          updatedBombs[j][x] = 12;
+        } else {
+          updatedBombs[j][x] = 11;
+        }
+      }
 
-        console.log(amount);
-    };
+      setBombs(updatedBombs);
+    }
+  };
 
-    let content = bombs.map((bomb, index) => {
+  let content = bombs.map((bomb, index) => {
+    return (
+      <ShowSquare bomb={bomb} key={index} j={index} handleClick={handleClick} />
+    );
+  });
 
-        return <ShowSquare bomb={bomb} key={index} j={index} handleClick={handleClick}/>
-    });
-
-    return (<div className="main">
-        {content}
-    </div>);
+  return (
+    <div className="main">
+      <button
+        onClick={() => setAction("bomba")}
+        disabled={action === "bomba" ? true : false}
+      >
+        Bomba
+      </button>
+      <button
+        onClick={() => setAction("bandeira")}
+        disabled={action === "bandeira" ? true : false}
+      >
+        Bandeira
+      </button>
+      {content}
+    </div>
+  );
 }
 
 export default App;
--- src/components/ShowSquare.js
+++ src/components/ShowSquare.js
@@ -1,22 +1,31 @@
+function ShowSquare({ bomb, handleClick, j, action }) {
+  let content = bomb.map((square, index) => {
+    return (
+      <div
+        key={index}
+        onClick={() => handleClick(index, j, square)}
+        className="square"
+        style={{
+          backgroundColor:
+            square !== 10 && square !== 9 && square !== 12 && square !== 11
+              ? "rgb(202, 202, 202)"
+              : "",
+          border:
+            square === 12 || square === 11 ? "1px solid red" : "1px solid",
+        }}
+      >
+        {square !== 9 &&
+        square !== 10 &&
+        square !== 0 &&
+        square !== 12 &&
+        square !== 11
+          ? square
+          : ""}
+      </div>
+    );
+  });
 
-function ShowSquare({ bomb, handleClick, j }) {
-    const handleBombClick = () => {
-        alert("morreu"); 
-    };
-
-    let content = bomb.map((square, index) => {
-        if (square === 0) {
-            return <div onClick={() => handleClick(index, j)} className="notBomb"></div>
-        }
-        else {
-            return <div onClick={handleBombClick} className="bomb"></div>
-        }
-    });
-
-
-    return <div>{content} </div >
-
-
+  return <div>{content}</div>;
 }
 
 export default ShowSquare;
